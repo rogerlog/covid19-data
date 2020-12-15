b6593478b5fa1750dac46d192002ba4da0d60f57
Roger Guimaraes <logroger@gmail.com>
2020-12-15 18:13:34 -0300

    Front-end e requisições API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { StylesProvider } from '@material-ui/styles'
 import { CssBaseline } from '@material-ui/core'
-import GlobalStyle from './commons/styles/global-styles'
+import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
 
 function App() {
   return (
     <StylesProvider injectFirst>
-<CssBaseline />
-<GlobalStyle />
-<div>
-  Rr
-</div>
-
+      <CssBaseline />
+      <GlobalStyle />
+      <Main />
     </StylesProvider>
   );
 }
